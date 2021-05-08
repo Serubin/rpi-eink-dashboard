b211db830b9a99dbc0c7eccba0b66326b8000dd0
Serubin <Serubin@serubin.net>
2021-05-08 16:09:03 -0700

    Add eink script

---
+++ eink.py
@@ -0,0 +1,24 @@
+from lib import epd7in5_HD
+from PIL import Image,ImageDraw,ImageFont
+
+import os
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    
+    logging.info('getting display, init and clear')
+    epd = epd7in5_HD.EPD()
+    epd.init()
+    epd.Clear()
+    image = Image.open('screenshot.png')
+
+    logging.info('sending image to display')
+    epd.display(epd.getbuffer(image))
+    time.sleep(2)
+
+if __name__ == '__main__':
+    main()
