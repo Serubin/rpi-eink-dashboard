d926b817d4c6d4225428b0df11d3d3d9553e7acf
Serubin <Serubin@serubin.net>
2021-05-08 17:40:21 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-
-magicmirror_dir=magicmirror/
+magicmirror_dir=magicmirror
 
 # Install magic mirror
-git clone https://github.com/MichMich/MagicMirror.git $magicmirror_dir
+echo
+echo "Cloning Magicmirror."
+git clone https://github.com/MichMich/MagicMirror.git --depth 1 $magicmirror_dir 2> /dev/null || echo "Skipping: Repo already cloned"
 
 cd $magicmirror_dir
-npm install
 
-cp -a ../magicmirror-files .
+echo
+echo "Installing magicmirror dependencies..."
 
-cd -
+npm install &> /dev/null
+
+cp -a ../magicmirror-files/* .
+
+cd - > /dev/null
+echo "done."
+
+sonos_module_dir=$magicmirror_dir/modules/MMM-Sonos
+cd $sonos_module_dir
+
+echo
+echo "Installing Sonos/Cast module dependencies..."
+npm install &> /dev/null
+echo "done."
+
+cd - > /dev/null
+
+echo "Finishing up."
